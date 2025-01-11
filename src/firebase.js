bdfc9f0f5d8f4ec3bf9d32d0624d3aadc11dbd07
RitikTech-101 <ritikkhare88@gmail.com>
2025-01-11 16:04:18 +0530

    first commit from clone repository

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from 'firebase/auth';
 
 // Access Firebase config from environment variables
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC4MmYRqdpl9Ew5aX4iW20h3NeMdhdd5P4",
+  authDomain: "todo-app-92a7d.firebaseapp.com",
+  projectId: "todo-app-92a7d",
+  storageBucket: "todo-app-92a7d.firebasestorage.app",
+  messagingSenderId: "664812526014",
+  appId: "1:664812526014:web:9f5d890f383c2a6ee701ef"
 };
 
 const app = initializeApp(firebaseConfig);
